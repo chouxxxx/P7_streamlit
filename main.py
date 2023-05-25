3c285e1de9967cf9a26f6029b91c3f2b1a88ae4d
chouxxxx <115968618+chouxxxx@users.noreply.github.com>
2023-05-25 21:08:51 +0200

    fix run_time, add notebook

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import shap
 import lightgbm
 import sklearn
 import imblearn
+import numpy as np
 
 st.write("joblib_version:", joblib.__version__)
 st.write("streamlit_version:", st.__version__)
@@ -18,12 +19,10 @@ st.title("Loan attribution tool")
 st.subheader("Select your client id.")
 
 # Get the user's information
-dff = pd.read_csv("df.csv")
-df = pd.read_csv("X_test.csv")
+df = pd.read_csv("guys.csv")
 client = st.sidebar.slider("Select a client number", 0, len(df)-1, 0)
 client = df.iloc[client]
 client = client.SK_ID_CURR
-#client_shap = dff[dff.SK_ID_CURR == client]
 client = df[df.SK_ID_CURR == client]
 st.write("Welcome, client id", client.SK_ID_CURR.values[0])
 
@@ -34,9 +33,13 @@ client_bis = client.copy()
 for i in major_features.name:
     client_bis[i] = st.number_input(i, value=client[i].values[0])
 
+@st.cache_resource
+def get_model():
+    return joblib.load("modelSb")
+
 # Prediction
 if st.button("Predict"):
-    model = joblib.load("modelSb")
+    model = get_model()
     result = model.predict(client_bis)[0]
     if result == 1:
         result = "Your loan was refused."
@@ -44,15 +47,20 @@ if st.button("Predict"):
         result = "Your loan was granted."
     else:
         result = "Error during prediction."
-    client_score = model.predict_proba(client_bis)[0]
-    st.write("Your score is " + str(slient_score))
+    client_score = np.round(model.predict_proba(client_bis)[0][0], 2)
+    st.write("Your score is " + str(client_score))
     st.write(result)
 
-#st.write(client[major_features.name])
-df_mean = dff[major_features.name].mean()
-df_mean.columns = ["Feature", "Average_Population"]
+@st.cache_data
+def get_avg():
+    dff = pd.read_csv("df.csv")
+    #client_shap = dff[dff.SK_ID_CURR == client]
+    # #st.write(client[major_features.name])
+    df_mean = dff[major_features.name].mean()
+    df_mean.columns = ["Feature", "Average_Population"]
+    return df_mean
 st.write("Characteristics of the average population:")
-st.write(df_mean)
+st.write(get_avg())
 
 shap_values = joblib.load("shapSb5")
 #shap_explainer = joblib.load("shap_xpl")
